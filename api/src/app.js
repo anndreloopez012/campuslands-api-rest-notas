2791e734fd06e0e9cef34774296b9b46aed32e36
Andre Lopez <andrelopez012@gmail.com>
2026-09-08 10:22:12 -0600

    feat(crud): implementa mvc y persistencia con mongodb

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { notaRouter } from "./routes/nota.routes.js";
+import {
+  manejarError,
+  rutaNoEncontrada,
+} from "./middlewares/error.middleware.js";
 
 const app = express();
 
@@ -7,5 +12,10 @@ app.use(express.json());
 app.get("/health", (req, res) => {
   res.status(200).json({ estado: "ok" });
 });
+
+app.use("/api/v1/notas", notaRouter);
+
+app.use(rutaNoEncontrada);
+app.use(manejarError);
 
 export default app;
